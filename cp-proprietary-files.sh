e9a52f01d9c674fae768640564e650f47a12f3a1
smoki3 <kevinbayer8@gmail.com>
2015-07-09 09:30:48 +0200

    Fix Path
    Change-Id: I2bef34321bc378134b2c8935de15291c5ca0b4e1

--- cp-proprietary-files.sh
+++ cp-proprietary-files.sh
@@ -2,7 +2,7 @@
 
 VENDOR=xiaomi
 DEVICE=ferrari
-INDIR=~/ferrari/system
+INDIR=/opt/ferrari/system
 OUTDIR=../../../vendor/$VENDOR/$DEVICE/proprietary
 
 COUNT=`cat proprietary-files.txt | grep -v ^# | grep -v ^$ | wc -l | awk {'print $1'}`
