90959bb26afef479cec36f7fde74ed8381fc38b2
k2wlxda <k2wlxda@gmail.com>
2015-07-17 12:43:42 +0530

    Remove source build audio.primary we are using prebuilt audio.primary lib so no need for source build lib.
    Change-Id: I21caadce068c1470c1ea5c848a66dc3a16c309f5

--- device.mk
+++ device.mk
@@ -66,7 +66,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
-    audio.primary.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
