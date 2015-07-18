021b4f93f9813a929e7ed4f3e67bc3000e418a35
k2wlxda <k2wlxda@gmail.com>
2015-07-18 18:37:51 +0530

    Thermal:Add in thermal config

--- device.mk
+++ device.mk
@@ -278,7 +278,8 @@ PRODUCT_COPY_FILES += \
 
 # Thermal config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8939-in.conf:system/etc/thermal-engine-8939-in.conf \
 
 # Proprietery Firmware
 PRODUCT_COPY_FILES += \
