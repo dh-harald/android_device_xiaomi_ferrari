bfd47526078513574c1016cb7cd9f3142feb4e4a
Peter Nyilas <github@criminal.hu>
2015-12-01 10:18:28 +0000

    Build audio from source

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
+    audio.primary.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
